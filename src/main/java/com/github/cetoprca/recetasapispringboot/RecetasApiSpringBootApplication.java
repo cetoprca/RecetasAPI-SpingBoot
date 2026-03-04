657dbc7352d147f72b30e0acf8b663b8755295ca
cetoprca <cetoprca@gmail.com>
2026-03-04 15:38:25 +0100

    initial commit

---
+++ src/main/java/com/github/cetoprca/recetasapispringboot/RecetasApiSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.github.cetoprca.recetasapispringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecetasApiSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecetasApiSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/cetoprca/recetasapispringboot/RecetasApiSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.cetoprca.recetasapispringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecetasApiSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
