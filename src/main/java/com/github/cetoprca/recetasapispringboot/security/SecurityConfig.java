8c61adaf4355cf6964c98cdf422214717af519b3
cetoprca <cetoprca@gmail.com>
2026-03-05 09:10:39 +0100

    basic swagger and security

---
+++ src/main/java/com/github/cetoprca/recetasapispringboot/security/SecurityConfig.java
@@ -0,0 +1,25 @@
+package com.github.cetoprca.recetasapispringboot.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+    
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http){
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .cors(AbstractHttpConfigurer::disable)
+                .formLogin(AbstractHttpConfigurer::disable)
+                .httpBasic(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> {
+                    auth.anyRequest().permitAll();
+                });
+
+        return http.build();
+    }
+}
