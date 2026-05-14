530ec2949118866727cf8e5952137c4ed9f9252e
cetoprca <cetoprca@gmail.com>
2026-05-14 17:03:23 +0200

    removed displayName from credentials DTO

--- src/main/java/com/github/cetoprca/recetasapispringboot/DTO/CredentialsDTO.java
+++ src/main/java/com/github/cetoprca/recetasapispringboot/DTO/CredentialsDTO.java
@@ -2,7 +2,6 @@ package com.github.cetoprca.recetasapispringboot.DTO;
 
 public record CredentialsDTO(
         String handle,
-        String password,
-        String displayName
+        String password
 ) {
 }
